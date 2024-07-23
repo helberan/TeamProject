620c768ea8b6da510995257230c808b7098024b1
tmuzikova <tereza.muzikova@gmail.com>
2024-07-23 19:46:51 +0200

    collapsed navbar styling added

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -1,24 +1,42 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './Header.css';
 
 export const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+  const handleNavClick = () => setExpanded(false);
+
   return (
     <div>
-      <Navbar expand='lg' className='bg-body-teriary' fixed='top'>
+      <Navbar
+        expand='lg'
+        className='bg-body-teriary'
+        fixed='top'
+        expanded={expanded}
+      >
         <Container>
           <Navbar.Brand>
-            <Link className='navbar-brand ' to='/team-project' id='logo'>
+            <Link className='navbar-brand' to='/team-project' id='logo'>
               PhysioReact
             </Link>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
+          <Navbar.Toggle
+            aria-controls='basic-navbar-nav'
+            onClick={handleToggle}
+          />
+          <Navbar.Collapse
+            id='basic-navbar-nav'
+            className={expanded ? 'show' : ''}
+          >
             <Nav className='me-auto justify-content-center flex-grow-1 pe-5 navlinks'>
               <Link
                 to='/team-project/services'
                 style={{ textDecoration: 'none' }}
                 className='nav-link'
+                onClick={handleNavClick}
               >
                 Naše služby
               </Link>
@@ -26,6 +44,7 @@ export const Header = () => {
                 to='/team-project/aboutus'
                 style={{ textDecoration: 'none' }}
                 className='nav-link'
+                onClick={handleNavClick}
               >
                 O nás
               </Link>
@@ -33,22 +52,24 @@ export const Header = () => {
                 to='/team-project/prices'
                 style={{ textDecoration: 'none' }}
                 className='nav-link'
+                onClick={handleNavClick}
               >
                 Ceník
               </Link>
-
               <Link
                 to='/team-project/contact'
                 style={{ textDecoration: 'none' }}
                 className='nav-link'
+                onClick={handleNavClick}
               >
                 Kontakt
               </Link>
             </Nav>
-            <Nav>
+            <Nav className='booking-nav'>
               <Link
                 to='/team-project/bookingform'
                 style={{ textDecoration: 'none' }}
+                onClick={handleNavClick}
               >
                 <div className='bookingform-btn'>Objednat se</div>
               </Link>
